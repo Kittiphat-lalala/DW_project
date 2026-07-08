6974a817d80dfbf55e37f1807662b24fed92a189
Kittiphat-lalala <kittiphat.l@kkumail.com>
2026-07-08 03:00:45 +0000

    update: pip

--- basic_command.sh
+++ basic_command.sh
@@ -1,6 +1,9 @@
 # add all changes to the staging area
 git add
+
 # commit the changes with a message
 git commit -m "Your commit message here"
+
 #push the change to the remote repository
 git push origin <remote-branch-name>
+
